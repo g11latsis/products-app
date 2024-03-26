9049d65858f49bebfc8c62239614b7e40e31d8e8
g11latsis <g11latsis@gmail.com>
2024-03-26 14:10:05 +0200

    jest user tests

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
 const mongoose = require('mongoose');
 
 app.use(express.json());
@@ -8,15 +8,22 @@ app.use(express.json());
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
 
+
 mongoose.connect(process.env.MONGODB_URI).then(
         () => {console.log("Connection with MongoDB enstablised...")},
         err => {console.log("Failed to connect to MongoDB!", err)}
     );
 
+const cors = require ('cors');
+app.use(cors({
+    origin:'*'
+}))
+
 
 const user = require('./routes/user.route');
 const userProduct = require('./routes/user.products.routes');
 
+app.use('/', express.static('files'))
 app.use('/api/users', user)
 app.use('/api/user-products',userProduct)
 
@@ -25,6 +32,9 @@ app.use('/api-docs',
     swaggerUI.setup(swaggerDocument.options)
     )
 
-app.listen(port, () => {
-    console.log("Server running...")
-});
+
+// app.listen(port, () => {
+//     console.log("Server running...")
+// });
+
+module.exports = app;
